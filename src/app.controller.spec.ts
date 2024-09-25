a118fa86e1e54bc20140c5713cd957a3e8961c23
Erika Morais <erikafconca@gmail.com>
2024-09-25 15:20:19 -0300

    comecando com nest criando controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World 10!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Hello World 10!');
     });
   });
 });
